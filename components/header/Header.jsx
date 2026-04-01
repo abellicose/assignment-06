fc23e07d4b726b4911722da9fbf7e6c6cb0866c8
Zubayer Hossain <zubayerhossain180@gmail.com>
2026-04-01 11:21:04 +0600

    First

---
+++ components/header/Header.jsx
@@ -0,0 +1,28 @@
+import styles from "./Header.module.css";
+import CartIcon from "../../assets/cart.svg";
+
+
+function Header({})
+{
+    return (
+        <header>
+            <nav>
+                <h2><a href="#">DigiTools</a></h2>
+                <ul>
+                    <li><a href="#products">Products</a></li>
+                    <li><a href="#features">Features</a></li>
+                    <li><a href="#pricing">Pricing</a></li>
+                    <li><a href="#testimonials">Testimonials</a></li>
+                    <li><a href="#fAQ">FAQ</a></li>
+                </ul>
+                <div className={styles.btnContainer}>
+                    <button className={styles.btnCart}><img src={CartIcon} alt="Shopping Cart Icon" /></button>
+                    <button className={styles.btnLogin}>Login</button>
+                    <button className="btn-primary">Get Started</button>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
